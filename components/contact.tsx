d1f51a499b8851216d6eda141d76675fab91258d
ProfMoriartey <61118848+ProfMoriartey@users.noreply.github.com>
2024-02-09 05:48:37 +0000

    Remove paragraph from contact section

--- components/contact.tsx
+++ components/contact.tsx
@@ -31,14 +31,6 @@ export default function Contact() {
     >
       <SectionHeading>Contact Us</SectionHeading>
 
-      <p className="text-gray-700 -mt-6 dark:text-white/80">
-        Please contact me directly at{" "}
-        <a className="underline" href="mailto:hangflow@gmail.com">
-          hangflow@gmail.com
-        </a>{" "}
-        or through this form.
-      </p>
-
       <form
         className="mt-10 flex flex-col dark:text-black"
         action={async (formData) => {
