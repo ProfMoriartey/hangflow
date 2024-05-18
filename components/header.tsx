0abe6476e883a5a475fb228a1e0c72fdc8ae5371
dangerous-chilli <kaito9814@gmail.com>
2024-05-18 14:56:19 +0300

    Add shadcn and translationg to the website. changed header to be translated but lost function, Adding shadcn chanfed the default set font

--- components/header.tsx
+++ components/header.tsx
@@ -3,7 +3,7 @@
 import Image from "next/image";
 import React from "react";
 import { motion } from "framer-motion";
-// import { links } from "@/lib/data";
+import { links } from "@/lib/data";
 import Link from "next/link";
 import clsx from "clsx";
 import { useActiveSectionContext } from "@/context/active-section-context";
@@ -67,46 +67,14 @@ export default function Header( {headerHome, headerAbout, headerEvents, headerBo
 
 {/* ssssssssssssssssssssssssssssssssssssssssssssssssssssssssss */}
 
-      <nav className="flex fixed top-[0.15rem] left-1/2 h-12 -translate-x-1/2 py-2 sm:top-[1.7rem] sm:h-[initial] sm:py-0">
-        <ul className="flex w-[22rem] flex-wrap items-center justify-center gap-y-1 text-[0.9rem] font-medium text-gray-500 sm:w-[initial] sm:flex-nowrap sm:gap-7">
-          {links.map((link) => (
-            <motion.li
-              className="h-3/4 flex items-center justify-center relative"
-              key={link.hash}
-              initial={{ y: -100, opacity: 0 }}
-              animate={{ y: 0, opacity: 1 }}
-            >
-              <Link
-                className={clsx(
-                  "flex w-full items-center justify-center px-3 py-3 hover:text-gray-950 transition dark:text-gray-400 dark:hover:text-gray-300",
-                  {
-                    "text-gray-950 dark:text-gray-200":
-                      activeSection === link.name,
-                  }
-                )}
-                href={link.hash}
-                onClick={() => {
-                  setActiveSection(link.name);
-                  setTimeOfLastClick(Date.now());
-                }}
-              >
-                {link.name}
-
-                {link.name === activeSection && (
-                  <motion.span
-                    className="bg-gray-100 rounded-full absolute inset-0 -z-10 dark:bg-gray-800"
-                    layoutId="activeSection"
-                    transition={{
-                      type: "spring",
-                      stiffness: 380,
-                      damping: 30,
-                    }}
-                  ></motion.span>
-                )}
-              </Link>
-            </motion.li>
-          ))}
-          <div className="flex flex-row m-3 gap-1 sm:invisible sm:absolute">
+      <nav className="flex fixed top-[0.15rem] left-[15%] sm:left-1/2 h-12 sm:-translate-x-1/2 py-2 sm:top-[1.7rem] sm:h-[initial] sm:py-0">
+        <ul className="flex w-[20rem] flex-wrap sm:items-center sm:justify-center gap-y-1 text-[0.9rem] font-medium text-gray-500 sm:w-[initial] sm:flex-nowrap sm:gap-7">
+          <li className="px-2 sm:p-2 mx-2">{headerHome}</li>
+          <li className="px-2 sm:p-2 mx-2">{headerAbout}</li>
+          <li className="px-2 sm:p-2 mx-2">{headerEvents}</li>
+          <li className="px-2 sm:p-2 mx-2">{headerBoard}</li>
+          <li className="px-2 sm:p-2 mx-2">{headerContact}</li>
+          <div className="flex flex-row ml-6 gap-1 sm:invisible sm:absolute">
             <div>
               <Link href={"/en"}>EN</Link>
             </div>
