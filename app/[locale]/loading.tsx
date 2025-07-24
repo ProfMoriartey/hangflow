dbc9d1f3d5e1fb49426c6a1c4e6b5cadb976d517
Ahmed Alhusaini <61118848+ProfMoriartey@users.noreply.github.com>
2025-07-24 09:47:13 +0300

    Update loading.tsx

--- app/[locale]/loading.tsx
+++ app/[locale]/loading.tsx
@@ -1,14 +1,8 @@
 export default function Loading() {
   return (
     // UPDATED: Use bg-background for the background
-    <div
-      className=(
-        "flex min-h-screen items-center justify-center",
-      
-      )
-    >
+    <div className="flex min-h-screen items-center justify-center">
       <div className="flex flex-col items-center">
-        {/* UPDATED: Use text-primary for the spinner color */}
         Loading . . . 
       </div>
     </div>
